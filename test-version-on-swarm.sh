c2cf730bae7a893a2727e2bce0f854a5e711b9b5
Chris Wendt <chrismwendt@gmail.com>
2015-12-09 09:26:21 -0800

    Update

--- test-version-on-swarm.sh
+++ test-version-on-swarm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. common.sh
+
 [ $# -eq 1 ] || (echo "Usage: $0 <plugin-dir>" && exit 1)
 
 PLUGIN_DIR=$1
@@ -12,4 +14,10 @@ done
 
 cd - > /dev/null
 cp $PLUGIN_DIR/build/distributions/elasticsearch-srv-discovery-$version.zip .
-./test-on-swarm.sh
+
+./rm-es.sh
+./mk-es.sh
+until [ $(curl -s $(./ip:port.sh es-1)/_nodes | jq ".nodes | length") = $ES_NODES ]; do
+    echo "Waiting for $ES_NODES to form a cluster"
+    sleep 1
+done
